9a60b854b667497989b055f358b726c3e1bc790e
valentina reale <valenreale123@gmail.com>
2024-12-01 13:30:45 -0300

    imagenes en webp y hydration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,21 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideClientHydration, withHttpTransferCacheOptions } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes),
-    provideHttpClient(), provideAnimationsAsync(),
+    provideHttpClient(
+    ),
+    provideAnimationsAsync(),
+    provideClientHydration(withHttpTransferCacheOptions({
+      includePostRequests: true
+    }))
   ]
 };
+function withTransferState(): import("@angular/common/http").HttpFeature<import("@angular/common/http").HttpFeatureKind> {
+  throw new Error('Function not implemented.');
+}
+
