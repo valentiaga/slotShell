8049b0b7d5cd322c650b467fe604584ad82af87e
Lautaro Luna <lauluna817@gmail.com>
2024-12-01 21:47:38 -0300

    agrego https al backend

--- src/assets/environment.ts
+++ src/assets/environment.ts
@@ -1,5 +1,5 @@
 export const environments = {
-    BASE_URL: `http://api.shell-suarez.com/api`,
+    BASE_URL: `https://api.shell-suarez.com/api`,
     // BASE_URL: `http://localhost:3000/api`,
 
     ID_ESTACION: 1,
