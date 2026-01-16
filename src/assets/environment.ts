06391727262f8d96fbb45b330d6562d3412772cc
Lautaro Luna <lauluna817@gmail.com>
2026-01-16 10:55:32 -0300

    Update environment.ts

--- src/assets/environment.ts
+++ src/assets/environment.ts
@@ -1,6 +1,6 @@
 export const environments = {
     // BASE_URL: `https://api.shell-suarez.com/api`,
     // BASE_URL: `http://localhost:3000/api`,
-    BASE_URL: `https://nsw-boring-penalty-cap.trycloudflare.com/api`,
+    BASE_URL: `https://licence-efficiently-wires-cache.trycloudflare.com/api`,
     ID_ESTACION: 1,
 }
