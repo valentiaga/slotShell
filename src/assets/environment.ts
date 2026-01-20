25568d2375174b8f8a3888192b03653728359427
Lautaro Luna <lauluna817@gmail.com>
2026-01-20 20:06:13 -0300

    Update environment.ts

--- src/assets/environment.ts
+++ src/assets/environment.ts
@@ -1,6 +1,6 @@
 export const environments = {
     // BASE_URL: `https://api.shell-suarez.com/api`,
     // BASE_URL: `http://localhost:3000/api`,
-    BASE_URL: `https://coral-tee-devices-decimal.trycloudflare.com/api`,
+    BASE_URL: `https://ran-epson-ericsson-moisture.trycloudflare.com/api`,
     ID_ESTACION: 1,
 }
