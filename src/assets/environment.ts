b76701c82646058d27f0d00f0215fe1f78c88450
Lautaro Luna <lauluna817@gmail.com>
2026-01-20 19:15:40 -0300

    Update environment.ts

--- src/assets/environment.ts
+++ src/assets/environment.ts
@@ -1,6 +1,6 @@
 export const environments = {
     // BASE_URL: `https://api.shell-suarez.com/api`,
     // BASE_URL: `http://localhost:3000/api`,
-    BASE_URL: `https://match-erp-latino-cabinet.trycloudflare.com/api`,
+    BASE_URL: `https://utility-graphs-around-against.trycloudflare.com/api`,
     ID_ESTACION: 1,
 }
