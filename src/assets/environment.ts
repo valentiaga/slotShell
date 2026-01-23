539ae217450666f969c3cc19e5abee7d92c5b584
Lautaro Luna <lauluna817@gmail.com>
2026-01-23 11:49:46 -0300

    Update environment.ts

--- src/assets/environment.ts
+++ src/assets/environment.ts
@@ -1,6 +1,6 @@
 export const environments = {
     // BASE_URL: `https://api.shell-suarez.com/api`,
     // BASE_URL: `http://localhost:3000/api`,
-    BASE_URL: `https://ran-epson-ericsson-moisture.trycloudflare.com/api`,
+    BASE_URL: `https://pal-journalism-animated-chemistry.trycloudflare.com/api`,
     ID_ESTACION: 1,
 }
